9cf9d3ed8b357603fe33b562a530d551702e3f35
Yana Agun Siswanto <yana.developer@gmail.com>
2022-06-01 02:29:09 +0700

    add mui

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+
+export default function App() {
+  return <Button variant="contained">eLocker Manager Application</Button>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
